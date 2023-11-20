36f5a3cb933b3f20406e1d35a8e8d24791be9364
MinhHien1999 <minhhien852123456@gmail.com>
2023-11-20 19:14:35 +0700

    set env

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const { appRouter } = require("./routes/apps.route");
+const dotenv = require("dotenv").config();
 
 const PORT = 3000;
 const DB_NAME = "Password-Manager";
@@ -13,9 +14,9 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.use(appRouter);
-
+// console.log(process.env.MONGO_URI);
 mongoose
-  .connect(`mongodb://localhost:27017/${DB_NAME}`)
+  .connect(`${process.env.MONGO_URI}`)
   .catch((error) => console.log("connection error: " + error));
 mongoose.connection.on("open", () => {
   console.log("mongodb connected");
